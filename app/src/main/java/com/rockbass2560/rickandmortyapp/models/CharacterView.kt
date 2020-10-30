193522cc59dd72b515c4efa145c73fa5a6ae5b99
Francisco Gonzalez Hernandez <franciscogonzalez_hernandez@hotmail.com>
2020-10-30 13:30:41 -0700

    Inicio

---
+++ app/src/main/java/com/rockbass2560/rickandmortyapp/models/CharacterView.kt
@@ -0,0 +1,10 @@
+package com.rockbass2560.rickandmortyapp.models
+
+data class CharacterView (
+    var image: String,
+    var name: String,
+    var status: String,
+    var specie: String,
+    var location: String,
+    var firstSeen: String
+)
